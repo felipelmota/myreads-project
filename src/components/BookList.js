058d4437f82ed4e5c6eb0ed9ae0f91d8c95fb1d8
Felipe Mota <felipelmota@gmail.com>
2018-03-02 00:24:53 -0300

    Ending entire components for project

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
 import Bookshelf from './Bookshelf'
+import * as BooksAPI from '../BooksAPI'
 
 class BookList extends Component {
 
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            this.setState({books: books})
+        })
+    }
+
     render() {
+        const { books } = this.state
+
         return(
             <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-                <Bookshelf />
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
+                <p> {books.length} </p>
+                <p> Teste </p>
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <Bookshelf books={books}/>
+                </div>
+                <div className="open-search">
+                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                </div>
             </div>
         )
     }
